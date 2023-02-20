2fe6489a88cc542b78270bed11dfe53a35b3deac
Misha Sternberg <mishasternberg@gmail.com>
2023-02-20 15:05:27 -0500

    This commit adds the necessary functionality to complete the freecodecamp challenge.

--- index.js
+++ index.js
@@ -24,9 +24,30 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  var date;
+  if (!req.params.date) {
+    date = new Date(parseInt(Date.now()));
+  }
+  else if (req.params.date.includes("-")) {
+    date = new Date(req.params.date);
+  }
+  else {
+    date = new Date(parseInt(req.params.date));
+  }
+
+  if(isNaN(date)){
+    res.json({"error": "Invalid Date"});
+  }else {
+    var unixTS = Math.floor(date.getTime());
+    date = date.toGMTString();
+    res.json({"unix": unixTS, "utc": date});
+  }
+})
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
